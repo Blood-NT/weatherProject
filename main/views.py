548ad060fa729aafaa6b1d428be8f4bf6c17a01a
Nolan <trongson2k@gmail.com>
2022-11-20 20:11:36 +0700

    done :)
    ok

--- main/views.py
+++ main/views.py
@@ -6,37 +6,37 @@ from django.shortcuts import render
 def index(request):
 
     if request.method == 'POST':
-        city = request.POST['city']
-        city = city.replace(" ", "")
+        try:
+            city = request.POST['city']
+            city = city.replace(" ", "")
 
-        source = urllib.request.urlopen('http://api.openweathermap.org/data/2.5/weather?q=' +
-                                        city + '&units=metric&appid=08962508c7447b773da61af9a67694b4').read()
-        list_of_data = json.loads(source)
-        # if (list_of_data["cod"] == "404"):
-        #     data = {
-        #         "country_code": "none",
-        #         "coordinate": "none",
-        #         "temp": "none",
-        #         "pressure": "none",
-        #         "humidity": "none",
-        #         'main': "none",
-        #         'description': "none",
-        #         'icon': "none",
-        #     }
-        # else:
-        data = {
-            "country_code": str(list_of_data['sys']['country']),
-            "coordinate": str(list_of_data['coord']['lon']) + ', '
-            + str(list_of_data['coord']['lat']),
+            source = urllib.request.urlopen('http://api.openweathermap.org/data/2.5/weather?q=' +
+                                            city + '&units=metric&appid=08962508c7447b773da61af9a67694b4').read()
+            list_of_data = json.loads(source)
+            data = {
+                "country_code": str(list_of_data['sys']['country']),
+                "coordinate": str(list_of_data['coord']['lon']) + ', '
+                + str(list_of_data['coord']['lat']),
 
-            "temp": str(list_of_data['main']['temp']) + ' °C',
-            "pressure": str(list_of_data['main']['pressure']),
-            "humidity": str(list_of_data['main']['humidity']),
-            'main': str(list_of_data['weather'][0]['main']),
-            'description': str(list_of_data['weather'][0]['description']),
-            'icon': list_of_data['weather'][0]['icon'],
-        }
-        print(data)
+                "temp": str(list_of_data['main']['temp']) + ' °C',
+                "pressure": str(list_of_data['main']['pressure']),
+                "humidity": str(list_of_data['main']['humidity']),
+                'main': str(list_of_data['weather'][0]['main']),
+                'description': str(list_of_data['weather'][0]['description']),
+                'icon': list_of_data['weather'][0]['icon'],
+            }
+            print(data)
+        except:
+            data = {
+                "country_code": "none",
+                "coordinate": "none",
+                "temp": "none",
+                "pressure": "none",
+                "humidity": "none",
+                'main': "none",
+                'description': "none",
+                'icon': "none",
+            }
     else:
         data = {}
 
